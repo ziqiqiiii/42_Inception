89c15a76f7ac54d7afa522a97dc4137a9f7fd560
tzi-qi <ziqi.thong@gmail.com>
2025-05-16 06:28:53 -0400

    feat (update) - split some from Dockerfile to bash script for modularity

---
+++ srcs/requirements/nginx/tools/nginx_init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#--------------Gnerating SSL Certificate--------------#
+echo "Generating SSL Certificate ... "
+openssl req -x509 -nodes -out /etc/nginx/ssl/inception.crt -keyout \
+    /etc/nginx/ssl/inception.key -subj "/C=MO/ST=KH/L=KH/O=42/OU=42/CN=$DOMAIN_NAME/UID=$WP_ADMIN_N"
+echo "Done"
+
+#--------------Start Nginx--------------#
+exec "$@";
